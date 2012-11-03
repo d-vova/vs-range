b54cba7b4cb540b1d876e77d30a510cab885f326
Vladimir <vladimir@blipboard.com>
2012-11-02 23:47:54 -0700

    updated range class to support multi range unite and intersect

---
+++ range.js
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2012 Vladimir's Software. All rights reserved.
+ *
+ * @fileoverview Range of numerical values that allows to specify, unite,
+ *               intersect, subtract, and invert intervals, as well as 
+ *               generate random values and compute corner case values
+ * @author vladimir.darmin@gmail.com
+ *
+ * @created Fri, Nov 02 2012 - 23:10:35 -0700
+ * @updated Fri, Nov 02 2012 - 23:10:35 -0700
+ */
+
+var Boundary = exports.Boundary = require('./Boundary');
+
+var Interval = exports.Interval = require('./Interval');
+
+var Range = exports.Range = require('./Range');
+
+exports.create = function create ( range ) {
+  return new Range(range);
+}
+
+exports.unite = function unite ( ) {
+  var ranges = [ ];
+  
+  for ( var i = 0; i < arguments.length; i += 1 ) {
+    var range = arguments[i];
+
+    ranges.push(range instanceof Range ? range : new Range(range));
+  }
+
+  return Range.unite(ranges);
+}
+
+exports.intersect = function intersect ( ) {
+  var ranges = [ ];
+  
+  for ( var i = 0; i < arguments.length; i += 1 ) {
+    var range = arguments[i];
+
+    ranges.push(range instanceof Range ? range : new Range(range));
+  }
+
+  return Range.intersect(ranges);
+}
+
+exports.invert = function invert ( range ) {
+  return (range instanceof Range ? range : new Range(range)).invert();
+}
